2e40315901e175c70ec326d8351fff6766b2ec57
Florian Gramß <6034322+gramss@users.noreply.github.com>
2022-12-11 19:53:28 +0100

    cleanup and fixing syntax

--- gqrx.rb
+++ gqrx.rb
@@ -4,27 +4,15 @@ class Gqrx < Formula
 
   depends_on 'cmake' => :build
   depends_on 'qt@5'
-  #depends_on 'boost'
   depends_on 'gnuradio'
   depends_on 'gramss/sdr/gr-osmosdr'
 
   def install
     mkdir "build" do
       ENV.append "CXXFLAGS", "-02"
-      #system "export CXXFLAGS=-O2"
-      
-      system "cmake", "..",
+      system "cmake", ".."
       system "make"
-      bin.install 'gqrx.app/Contents/MacOS/gqrx'
-      
+      bin.install 'gqrx.app/Contents/MacOS/gqrx'    
     end
-      
-      
-      
-    #system "qmake -set PKG_CONFIG /usr/local/bin/pkg-config"
-    #system "qmake -query"
-    #system "qmake gqrx.pro"
-    #system "make"
-    #bin.install 'gqrx.app/Contents/MacOS/gqrx'
   end
 end
